93736b38fbbb6600c33c0d4d489d3c68bc22be22
Syed Niddal Ali <niddalalii@gmail.com>
2025-10-07 04:02:56 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -74,7 +74,7 @@ class MyApp extends StatelessWidget {
             ),
         ],
         child: MaterialApp(
-          title: 'Flutter Nemo',
+          title: 'Flutter Demo',
           theme: ThemeData(
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
             useMaterial3: true,
